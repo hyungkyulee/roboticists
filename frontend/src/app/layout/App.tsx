e43b6da72b9ceedba396fdd3ef0f398aad376b0c
hyungkyu <hyungkyu@deepeyes.co.uk>
2020-03-15 16:13:29 +0000

    react hook with useState and useEffect replacing the class to the function

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -1,44 +1,34 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
 import './styles.css';
 import axios from 'axios';
 import { IPlayer } from '../../models/player';
 
-interface IState {
-  players: IPlayer[] 
-}
-class App extends Component<{}, IState> {
-  state: IState = {
-    players: [],
-  }
+const App = () => {
 
-  componentDidMount() {
+  const [players, setPlayers] = useState<IPlayer[]>([]);
+
+  useEffect(() => {
     axios.get<IPlayer[]>('http://localhost:5000/api/players')
       .then(res => {
-        console.log(res)  
-        this.setState({
-          players: res.data,
-        })
-    })
+        setPlayers(res.data)
+      })
+  }, []);
 
-  }
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='puzzle' />
+        <Header.Content>Robot Premier League - Base Station</Header.Content>
+      </Header>
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='puzzle' />
-          <Header.Content>Robot Premier League - Base Station</Header.Content>
-        </Header>
-
-        <List>
-          {this.state.players.map((player) => (
-            <List.Item key={player.id}>{player.name}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
+      <List>
+        {players.map((player) => (
+          <List.Item key={player.id}>{player.name}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
